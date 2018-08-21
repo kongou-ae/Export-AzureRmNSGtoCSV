1e979a3d8b8745c7c86874e97c4741917689af96
kongou-ae <y.matsumoto.ae@gmail.com>
2018-08-21 23:15:03 +0900

    Update Export-AzureRmNSGtoCSV.ps1

--- Export-AzureRmNSGtoCSV.ps1
+++ Export-AzureRmNSGtoCSV.ps1
@@ -4,7 +4,7 @@ Login-AzureRmAccount
 $subscriptionId = (Get-AzureRmSubscription | Out-GridView -PassThru).SubscriptionId
 Select-AzureRmSubscription -Subscription $subscriptionId
 
-$now = Get-Date -Format yyyyMMdd-hhmm
+$now = Get-Date -Format yyyyMMdd-HHmm
 $folder = New-Item -ItemType Directory $HOME/$now
 Write-Output "Generated $($folder.FullName)"
 
@@ -13,7 +13,7 @@ $NSGs = Get-AzureRmNetworkSecurityGroup
 foreach($NSG in $NSGs){
     $securityRules = New-Object  System.Collections.ArrayList
 
-    foreach($rule in $NSG.SecurityRules | Sort-Object Priority,Direction ){
+    foreach($rule in $NSG.SecurityRules ){
         $securityRules.Add($rule) > $null
     }
 
@@ -23,16 +23,17 @@ foreach($NSG in $NSGs){
 
     $CsvName = $HOME + "/" + $now + "/" + $NSG.Name + ".csv"
 
+    $securityRules = $securityRules | Sort-Object -Property Direction,Priority
     $securityRules | Select-Object `
         Direction,
         Priority,
+        Name,
         @{Label="SourceAddressPrefix"; Expression={ $_.SourceAddressPrefix -join ","}},
         @{Label="SourcePortRange"; Expression={ $_.SourcePortRange -join ","}},
         @{Label="DestinationAddressPrefix"; Expression={ $_.DestinationAddressPrefix -join ","}},
-        @{Label="DestinationPortRange"; Expression={ $_.DestinationPortRange -join ","}},
         Protocol,
-        Access,
-        Description | Export-Csv -NoTypeInformation -Path $CsvName
+        @{Label="DestinationPortRange"; Expression={ $_.DestinationPortRange -join ","}},
+        Access | Export-Csv -NoTypeInformation -Path $CsvName
 
     Write-Output "Generated $CsvName"
 }
